aa77c9ce0f8d0b618bdf528447bf9f17a5511844
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-02-25 10:00:24 -0300

    add: interface basic example

---
+++ interfaces/01-car_rental-no_interface/src/model/services/RentalService.java
@@ -0,0 +1,43 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    // Not a good practice to declare BrazilTaxService in RentalService, because
+    // it becomes dependent on this specific tax service. If in the future the type
+    // of tax service (country in this case) changes, we'll have 2 alteration points.
+    // Instead we should use an Interface (TaxService).
+
+    // TL;DR: BrazilTaxService here is bad practice because code maintenance will be bad.
+    // Use Interface instead.
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+        long t1 = carRental.getStart().getTime();
+        long t2 = carRental.getFinish().getTime();
+        double hours = (double)(t2 - t1) / 1000 / 60 / 60;
+
+        double basicPayment;
+        if (hours <= 12.0) {
+            basicPayment = pricePerHour * Math.ceil(hours);
+        }
+        else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24);
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
