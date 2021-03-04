93f53a74ba89777ad84a2747d63eb79d050d9299
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-04 15:37:48 -0300

    add: Department and Seller entity classes

--- jdbc/07-jdbc-dao_project/src/application/Program.java
+++ jdbc/07-jdbc-dao_project/src/application/Program.java
@@ -1,10 +1,9 @@
 package application;
 
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
+import model.entities.Department;
+import model.entities.Seller;
 
-import java.sql.*;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
@@ -40,7 +39,12 @@ the object Factory will be responsible for instantiating DAO implementations.
 
         // DaoFactory: responsible for instantiating the DAOs
 
+        Department obj = new Department(1, "Books");
 
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(obj);
+        System.out.println(seller);
 
     }
 }
