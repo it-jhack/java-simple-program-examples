192bdd8632072ba7ff6fe19cb62f2957062611e9
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-05 07:41:17 -0300

    add (DAO jdbc project): findById() findByDepartment()

--- jdbc/07-jdbc-dao_project/src/application/Program.java
+++ jdbc/07-jdbc-dao_project/src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -39,12 +42,24 @@ the object Factory will be responsible for instantiating DAO implementations.
 
         // DaoFactory: responsible for instantiating the DAOs
 
-        Department obj = new Department(1, "Books");
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println(obj);
+        System.out.println("===== TEST 1: seller findById =====");
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+
+        System.out.println("\n===== TEST 2: seller findByDepartment =====");
+        // Beware to NOT instantiate a department for each seller
+        // Instead the department should be instantiated only once,
+        // and then sellers point to that department.
+
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
 
     }
 }
---
+++ jdbc/07-jdbc-dao_project/src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,175 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import db.DB;
+import db.DbException;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn; // connection dependency
+    // NOTE:
+    // DAO object can do more than one operation, so close the connection on the main Program.
+
+    public SellerDaoJDBC(Connection conn) { // forcing connection dependency injection
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Seller obj) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            // no need to instantiate 'conn' inside methods, because
+            // already instantiated as a dependency.
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE seller.Id = ?");
+
+            // previous placeholder receives 'id' from parameter
+            st.setInt(1, id);
+            // executing and storing sql command
+            rs = st.executeQuery();
+            // ResultSet table from query will be structured as:
+            // Id | Name | Email | BirthDate | BaseSalary | DepartmentId | DepName
+
+            // tests if there's any result from previous sql command
+            // otherwise 'rs.next() = false' and returns null
+            if (rs.next()) {
+                // transforming sql table from query into associated objects (Department and Seller)
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
+                return obj;
+            }
+            return null;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+            // Do not close connection in the method, because the same DAO object can do more
+            // than one operation. Close the connection on the main Program.
+        }
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        // throws SQLException because methods that call instantiateDepartment() already treat it
+
+        // transforming sql table from query into associated objects (Department and Seller)
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        // throws SQLException because methods that call instantiateDepartment() already treat it
+
+        // transforming sql table from query into associated objects (Department and Seller)
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        // Department is the associated object, NOT the id from table:
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<Seller> findByDepartment(Department department) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE DepartmentId = ? "
+                            + "ORDER BY Name");
+
+            st.setInt(1, department.getId());
+            rs = st.executeQuery();
+
+            // List<Seller> because there can be more than one result
+            List<Seller> list = new ArrayList<>();
+            // helping point sellers to the same Department object
+            Map<Integer, Department> map = new HashMap<>();
+
+            // 'while' because there can be more than one result
+            while (rs.next()) {
+                // // WRONG: creating diff Department obj for each seller
+                // Department dep = instantiateDepartment(rs);
+                // Seller obj = instantiateSeller(rs, dep);
+                // list.add(obj);
+
+                // ===============
+
+                // CORRECT: pointing sellers to the same Department obj
+                // There are many ways to do it, but with 'map' we'll have
+                // better code maintenance options in the future.
+
+                // testing if 'Department dep' already exists
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                // instantiating, if 'Department dep' does NOT exist
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    // storing instantiated 'dep' into 'map'
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+            // Do not close connection in the method, because the same DAO object can do more
+            // than one operation. Close the connection on the main Program.
+        }
+    }
+}
