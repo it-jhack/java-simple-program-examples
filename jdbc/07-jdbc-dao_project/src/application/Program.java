10e4a2e52927c918baba3d31b985bf3d32a109e4
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-05 09:21:35 -0300

    add (DAO jdbc project): seller insert()

--- jdbc/07-jdbc-dao_project/src/application/Program.java
+++ jdbc/07-jdbc-dao_project/src/application/Program.java
@@ -45,12 +45,12 @@ the object Factory will be responsible for instantiating DAO implementations.
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("===== TEST 1: seller findById =====");
+        System.out.println("===== TEST 1: seller findById() =====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
 
-        System.out.println("\n===== TEST 2: seller findByDepartment =====");
+        System.out.println("\n===== TEST 2: seller findByDepartment() =====");
         // Beware to NOT instantiate a department for each seller
         // Instead the department should be instantiated only once,
         // and then sellers point to that department.
@@ -62,11 +62,17 @@ the object Factory will be responsible for instantiating DAO implementations.
         }
 
 
-        System.out.println("\n===== TEST 3: seller findAll =====");
+        System.out.println("\n===== TEST 3: seller findAll() =====");
         list = sellerDao.findAll();
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+
+        System.out.println("\n===== TEST 4: seller insert() =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Seller inserted! New seller's id = " + newSeller.getId());
 
     }
 }
