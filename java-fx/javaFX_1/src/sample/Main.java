935d2bbd13abc35852588f29f88262e6313edb15
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-08 08:45:48 -0300

    refactor (java-fx): Main.java and comments

--- java-fx/javaFX_1/src/sample/Main.java
+++ java-fx/javaFX_1/src/sample/Main.java
@@ -7,15 +7,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    // In Application you can add:
+    // 'init' = things to do/start before your application
+    // 'start' = mandatory
+    // 'stop' = things to do after your application
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void start(Stage primaryStage) {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
 
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
