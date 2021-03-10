79f02a9c00a02f913c11e82c47b7d64f5eb9f1df
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-10 15:01:13 -0300

    update (javaFX_jdbc_workshop): saving new Seller

--- java-fx-jdbc/javaFX_jdbc_workshop/src/gui/SellerFormController.java
+++ java-fx-jdbc/javaFX_jdbc_workshop/src/gui/SellerFormController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
@@ -126,6 +127,27 @@ public class SellerFormController implements Initializable {
         }
         obj.setName(txtName.getText());
 
+        if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+            exception.addError("email", "Field can't be empty");
+        }
+        obj.setEmail(txtEmail.getText());
+
+        if (dpBirthDate.getValue() == null) {
+            exception.addError("birthDate", "Field can't be empty");
+        }
+        else {
+            Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+            obj.setBirthDate(Date.from(instant));
+        }
+
+        if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+            exception.addError("baseSalary", "Field can't be empty");
+        }
+        obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
+        // Setting/associating Department to seller
+        obj.setDepartment(comboBoxDepartment.getValue());
+
         if (exception.getErrors().size() > 0) {
             throw exception;
         }
@@ -180,7 +202,7 @@ public class SellerFormController implements Initializable {
     // Call DepartmentService, get departments from db, and load them to our list
     public void loadAssociatedObjects() {
         if (departmentService == null) {
-            throw new IllegalStateException("DepartmentService was null");
+            throw new IllegalStateException("DepaortmentService was null");
         }
         List<Department> list = departmentService.findAll();
         obsList = FXCollections.observableArrayList(list);
@@ -190,9 +212,13 @@ public class SellerFormController implements Initializable {
     private void setErrorMessages(Map<String, String> errors) {
         Set<String> fields = errors.keySet();
 
-        if (fields.contains("name")) {
-            labelErrorName.setText(errors.get("name"));
-        }
+        // Ternary operators instead of 'if else' for every single param
+        // label = error ? return label : return ""
+        // (returns label or sets empty string to label that had error)
+        labelErrorName.setText((fields.contains("name") ? errors.get("name") : ""));
+        labelErrorEmail.setText((fields.contains("email") ? errors.get("email") : ""));
+        labelErrorBirthDate.setText((fields.contains("birthDate") ? errors.get("birthDate") : ""));
+        labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : ""));
     }
 
     private void initializeComboBoxDepartment() {
