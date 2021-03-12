c75391a1b9ea5fccc7a49c4735d6d6d4190a8ab8
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-12 09:22:14 -0300

    update (web-serv-jpa-hib): user delete

--- web-services-jpa-hibernate/course/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ web-services-jpa-hibernate/course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,11 +36,20 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	// Creating user, retrieving 201 response
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
+		// 201 response == created: requires header informing location of the created resource
 		return ResponseEntity.created(uri).body(obj);
+		// return ResponseEntity.ok().body(obj); // Generic 200 response
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
