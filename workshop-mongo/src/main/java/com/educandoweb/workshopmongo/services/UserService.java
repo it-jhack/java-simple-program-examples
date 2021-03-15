b3b40ba13b500e52edcd9b13d75e5a0b79d487a9
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-15 07:41:59 -0300

    update (mongodb): POST user

--- workshop-mongo/src/main/java/com/educandoweb/workshopmongo/services/UserService.java
+++ workshop-mongo/src/main/java/com/educandoweb/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.UserDTO;
 import com.educandoweb.workshopmongo.repository.UserRepository;
 import com.educandoweb.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,17 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	// FAQ: Doesn't this violate the single-responsibility principle?
+	// Depending from the situation (not this case, at least yet) to instance a User,
+	// I may need to access the db, and UserService has dependency for it. So this
+	// makes it easier for a possible maintenance in the future to access the db.
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
