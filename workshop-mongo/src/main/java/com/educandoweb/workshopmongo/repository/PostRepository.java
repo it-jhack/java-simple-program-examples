fe4e39d94ae9eae29bda74751b6ea9c923c6e331
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-03-16 06:46:49 -0300

    update (mongodb): simple search with @query

--- workshop-mongo/src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
+++ workshop-mongo/src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,18 @@ package com.educandoweb.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.educandoweb.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	// @Query allows to search mongodb in the form of JSON
+	// https://docs.mongodb.com/manual/reference/operator/query/regex/
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
